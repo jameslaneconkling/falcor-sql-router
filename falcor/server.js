773e81e4ee8c75395aa71297fa09ccca4061c603
James Conkling <james.lane.conkling@gmail.com>
2016-07-14 18:59:47 -0400

    add subfolders route

--- falcor/server.js
+++ falcor/server.js
@@ -1,16 +1,18 @@
 const FalcorServer = require('falcor-express');
 const Router = require('falcor-router');
 const falcor = require('falcor');
+const R = require('ramda');
 const Rx = require('rx');
 const db = require('../db');
 const $ref = falcor.Model.ref;
 const $error = falcor.Model.error;
 
 const BaseRouter = Router.createClass([
-  // GET Folder by IDs
+  // GET Folders by IDs
   {
     route: "foldersById[{keys:ids}][{keys:fields}]",
     get(pathSet) {
+      console.log(JSON.stringify(pathSet));
       const ids = pathSet.ids;
       const fields = pathSet.fields;
 
@@ -57,16 +59,17 @@ const BaseRouter = Router.createClass([
         }, []);
     }
   },
-  // GET Folder from folderList by index
+  // GET Folders from folderList by index
   {
-    route: "folderList[{integers:indices}][{keys:fields}]",
+    route: "folderList[{integers:indices}]",
     get(pathSet) {
+      console.log(JSON.stringify(pathSet));
       const indices = pathSet.indices;
       const fields = pathSet.fields;
 
       return Rx.Observable.create(observer => {
         // node-sqlite only returns ROWID if it's aliased as something else
-        db.all(`SELECT ROWID as rowid, id, ${fields.join(', ')} FROM folder WHERE rowid IN (${indices.join(', ')})`, [], (err, rows) => {
+        db.all(`SELECT ROWID as rowid, id FROM folder WHERE rowid IN (${indices.join(', ')})`, [], (err, rows) => {
           if (err) {
             observer.onError(err);
           } else {
@@ -87,95 +90,133 @@ const BaseRouter = Router.createClass([
             value: $error(err.message)
           });
         })
-        .reduce((accumulator, data) => {
+        .map(data => {
           // if row doesn't exist, return null pathValue
           if (!data.row) {
-            const missingRowPathValue = {
+            return {
               path: ['folderList', data.idx],
               value: null
             };
-            return [...accumulator, missingRowPathValue];
           }
 
           // return pathValue ref to folder
-          const pathValueRef = {
+          return {
             path: ['folderList', data.idx],
             value: $ref(['foldersById', data.row.id])
           };
-
-          // return fields by pathValue
-          const pathValuesByField = fields.map(field => ({
-            path: ['foldersById', data.row.id, field],
-            value: data.row[field]
-          }));
-
-          return [...accumulator, pathValueRef, ...pathValuesByField];
         }, []);
+    }
+  },
+  // GET Folders with fields from folderList by index [optimized]
+  // {
+  //   route: "folderList[{integers:indices}][{keys:fields}]",
+  //   get(pathSet) {
+  //     console.log(JSON.stringify(pathSet));
+  //     const indices = pathSet.indices;
+  //     const fields = pathSet.fields;
+
+  //     return Rx.Observable.create(observer => {
+  //       // node-sqlite only returns ROWID if it's aliased as something else
+  //       db.all(`SELECT ROWID as rowid, id, ${fields.join(', ')} FROM folder WHERE rowid IN (${indices.join(', ')})`, [], (err, rows) => {
+  //         if (err) {
+  //           observer.onError(err);
+  //         } else {
+  //           indices.forEach(idx => {
+  //             observer.onNext({
+  //               idx,
+  //               row: rows.find(row => row.rowid === idx)
+  //             });
+  //           });
+  //           observer.onCompleted();
+  //         }
+  //       });
+  //     })
+  //       .catch(err => {
+  //         console.error(err);
+  //         return Rx.Observable.throw({
+  //           path: ['folderList'],
+  //           value: $error(err.message)
+  //         });
+  //       })
+  //       .reduce((accumulator, data) => {
+  //         // if row doesn't exist, return null pathValue
+  //         if (!data.row) {
+  //           const missingRowPathValue = {
+  //             path: ['folderList', data.idx],
+  //             value: null
+  //           };
+  //           return [...accumulator, missingRowPathValue];
+  //         }
+
+  //         // return pathValue ref to folder
+  //         const pathValueRef = {
+  //           path: ['folderList', data.idx],
+  //           value: $ref(['foldersById', data.row.id])
+  //         };
+
+  //         // return fields by pathValue
+  //         const pathValuesByField = fields.map(field => ({
+  //           path: ['foldersById', data.row.id, field],
+  //           value: data.row[field]
+  //         }));
+
+  //         return [...accumulator, pathValueRef, ...pathValuesByField];
+  //       }, []);
+  //   }
+  // },
+  // GET Subfolders from folders
+  {
+    route: "foldersById[{keys:parentIds}].folders[{integers:indices}]",
+    get(pathSet) {
+      console.log(JSON.stringify(pathSet));
+      const parentIds = pathSet.parentIds;
+      const indices = pathSet.indices;
+
+      // TODO - b/c sqlite doesn't support window functions, this queries the db separately...
+      const getFolderSubfolders = (childIndices, parentId) => {
+        return Rx.Observable.create(observer => {
+          // NOTE: this is terribly inefficient - grabs all rows up to the
+          db.all(`SELECT child.id as id, child.parentId as parentId
+                  FROM (SELECT * FROM folder WHERE id = ${parentId}) as parent
+                  JOIN folder as child
+                  ON parent.id = child.parentId
+                  LIMIT ${indices[indices.length -1]} OFFSET ${indices[indices[0]]}`, [], (err, rows) => {
+            if (err) {
+              observer.onError(err);
+            } else {
+              childIndices.forEach(idx => {
+                observer.onNext({
+                  idx,
+                  parentId,
+                  row: rows[idx]
+                });
+              });
+              observer.onCompleted();
+            }
+          });
+        })
+      };
+
+      return Rx.Observable.from(parentIds)
+        .concatMap(R.curry(getFolderSubfolders)(indices))
+        .map(data => {
+          // if row doesn't exist, return null pathValue
+          if (!data.row) {
+            return {
+              path: ['foldersById', data.parentId, 'folders', data.idx],
+              value: null
+            };
+          }
+
+          // return pathValue ref to folder
+          return {
+            path: ['foldersById', data.row.parentId, 'folders', data.idx],
+            value: $ref(['foldersById', data.row.id])
+          };
+        })
     }
   }
 ]);
-  // {
-  //   route: "foldersById[{keys:parentIds}].contains[{integers:indices}][{keys:childFields}]",
-  //   get(pathSet) {
-  //     return Rx.Observable.create(observer => {
-  //       const parentIds = pathSet.parentIds;
-  //       const indices = pathSet.indices;
-  //       const childFields = pathSet.childFields;
-
-  //       // NOTE: does not limit number of children per parent folder.  would need to GROUP BY parent.id and subselect by rowid?
-  //       //       also does not allow for fields on the parent...
-  //       //       also, need to make sure child order is consistent?  ORDER BY rowid?
-  //       db.all(`SELECT ${childFields.map(f => `child.${f}`).join(', ')}
-  //               FROM (SELECT * FROM folder WHERE id IN (${parentIds.join(', ')})) as parent
-  //               JOIN folder as child
-  //               ON parent.id = child.parentId`, [], (err, rows) => {
-  //         if (err) {
-  //           console.error(err);
-  //           observer.onError({
-  //             path: ['foldersById'],
-  //             value: $error(err.message)
-  //           });
-  //           return;
-  //         }
-
-  //         parentIds.forEach(parentId => {
-  //           const parentIdsChildren = rows.filter(row => row['child.parentId'] === parentId);
-
-  //           if (parentIdsChildren.length === 0) {
-  //             // parentIdFolder doesn't have children
-  //             observer.onNext({
-  //               path: ['foldersById', parentId, 'contains'],
-  //               value: null // should be []?  But router resolves [] incorrectly
-  //             });
-  //           } else {
-  //             parentIdsChildren.forEach((child, idx) => {
-  //               const childId = child['child.id'];
-
-  //               // add ref to folderById
-  //               observer.onNext({
-  //                 path: ['foldersById', parentId, 'contains', idx],
-  //                 value: $ref(['foldersById', childId])
-  //               });
-
-  //               // add fields to folder
-  //               // NOTE: should be able to abstract adding multiple fields via many PathValues
-  //               //       if only there was a PathSetValues type: that represents all values within one pathSet...
-  //               childFields.forEach(field => {
-  //                 observer.onNext({
-  //                   path: ['foldersById', childId, field],
-  //                   value: child[`child.${field}`]
-  //                 });
-  //               });
-  //             });
-  //           }
-  //         });
-
-  //         observer.onCompleted();
-  //       });
-  //     });
-  //   }
-  // },
-// ]);
 
 // To subclass:
 // const SubRouter = function(prop) {
