70f6233769ccc6bc272ee04b994425d07eb742c3
James Conkling <james.lane.conkling@gmail.com>
2016-08-17 19:01:49 -0400

    change process.env.ENV > env.NODE_ENV

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
 const dbFile = './db.sql';
-const db = process.env.ENV === 'dev' ?
+const db = process.env.NODE_ENV === 'development' ?
             new sqlite3.Database(':memory:') :
             new sqlite3.Database(dbFile);
 
-if (process.env.ENV === 'dev') {
+if (process.env.NODE_ENV === 'development') {
   require('./init')(db);
   require('./seed')(db);
 }
