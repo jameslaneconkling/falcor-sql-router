2155c3ad35d4aee120dfc0cf94d43e0f66ffd729
James Conkling <james.lane.conkling@gmail.com>
2016-09-24 02:37:13 -0400

    bugfix: offset list calls by correct amount

--- utils/falcor.js
+++ utils/falcor.js
@@ -8,4 +8,4 @@ exports.range2List = range => R.range(range.from, range.to + 1);
 /**
  * Convert falcor range to SQL OFFSET and LIMIT values
  */
-exports.range2LimitOffset = range => ({limit: range.to - range.from + 1, offset: range.from - 1});
+exports.range2LimitOffset = range => ({limit: range.to - range.from + 1, offset: range.from });
