2185a4f9f44cb46202950c979988c829507fc793
James Conkling <james.lane.conkling@gmail.com>
2016-09-24 02:36:50 -0400

    add new tests

--- test/folderById.js
+++ test/folderById.js
@@ -16,10 +16,11 @@ module.exports = () => {
     });
   });
 
+
   test('["foldersById", ...] Should return folders with ID 1, 3, and 4', assert => {
     const method = 'get';
     const paths = [
-      ["foldersById", [1, {"from": 3, "to": 4}], ["id", "name", "parentId"]]
+      ["foldersById", [1, 3, 4], ["id", "name", "parentId"]]
     ];
     const expectedResponse = {
       "jsonGraph": {
@@ -38,6 +39,82 @@ module.exports = () => {
             "id": 4,
             "name": "folder3",
             "parentId": 1
+          }
+        }
+      }
+    };
+
+    request(app)
+      .get(`/api/model.json?method=${method}&paths=${JSON.stringify(paths)}`)
+      .end((err, res) => {
+        assert.deepEqual(res.body, expectedResponse);
+        assert.end();
+      });
+  });
+
+
+  test('["foldersById", ...] Should return null for folder that doesn\'t exist', assert => {
+    const method = 'get';
+    const paths = [
+      ["foldersById", "nope", ["id", "name", "parentId"]]
+    ];
+    const expectedResponse = {
+      "jsonGraph": {
+        "foldersById": {
+          "nope": null
+        }
+      }
+    };
+
+    request(app)
+      .get(`/api/model.json?method=${method}&paths=${JSON.stringify(paths)}`)
+      .end((err, res) => {
+        assert.deepEqual(res.body, expectedResponse);
+        assert.end();
+      });
+  });
+
+
+  test('["foldersById", ...] Should return folder with sub folders', assert => {
+    const method = 'get';
+    const paths = [
+      ["foldersById", [1, 2], "name"],
+      ["foldersById",[1, 2], "folders", {"to":2}, ["id", "name", "parentId"]]
+    ];
+    const expectedResponse = {
+      "jsonGraph": {
+        "foldersById": {
+          "1": {
+            "name": "root folder",
+            "id": 1,
+            "folders": {
+              "0": { "$type": "ref", "value": [ "foldersById", 2] },
+              "1": { "$type": "ref", "value": [ "foldersById", 3] }
+            }
+          },
+          "2": {
+            "name": "folder1",
+            "id": 2,
+            "parentId": 1,
+            "folders": {
+              "0": { "$type": "ref", "value": [ "foldersById", 5] },
+              "1": { "$type": "ref", "value": [ "foldersById", 6] }
+            }
+          },
+          "3": {
+            "id": 3,
+            "name": "folder2",
+            "parentId": 1
+          },
+          "5": {
+            "id": 5,
+            "name": "folder1.1",
+            "parentId": 2
+          },
+          "6": {
+            "id": 6,
+            "name": "folder1.2",
+            "parentId": 2
           }
         }
       }
--- test/folderList.js
+++ test/folderList.js
@@ -6,6 +6,8 @@ const appConstructor = require('../app');
 module.exports = () => {
   // TODO - these probably need callbacks to ensure they complete before tests run
   // TODO - simplify dbConstructor so it can be passed a config: in-memory/file seedSQL/not
+  // TODO - import/build client falcor parsing lib to recreate response
+  // TODO - switch to tap diff: https://github.com/axross/tap-diff
   const db = dbConstructor();
   const app = appConstructor(db);
 
@@ -18,6 +20,7 @@ module.exports = () => {
     });
   });
 
+
   test('["folderList", ...] Should return folders at index 1 and 2', assert => {
     const method = 'get';
     const paths = [
@@ -26,20 +29,43 @@ module.exports = () => {
     const expectedResponse = {
       "jsonGraph": {
         "folderList": {
-          "1": {
-            "$type": "ref",
-            "value": [
-              "foldersById",
-              1
-            ]
-          },
+          "1": { "$type": "ref", "value": [ "foldersById", 2 ] },
+          "2": { "$type": "ref", "value": [ "foldersById", 3 ] }
+        },
+        "foldersById": {
           "2": {
-            "$type": "ref",
-            "value": [
-              "foldersById",
-              2
-            ]
+            "id": 2,
+            "name": "folder1",
+            "parentId": 1
+          },
+          "3": {
+            "id": 3,
+            "name": "folder2",
+            "parentId": 1
           }
+        }
+      }
+    };
+
+    request(app)
+      .get(`/api/model.json?method=${method}&paths=${JSON.stringify(paths)}`)
+      .end((err, res) => {
+        assert.deepEqual(res.body, expectedResponse);
+        assert.end();
+      });
+  });
+
+
+  test('["folderList", ...] Should return folders at index 0 and 1', assert => {
+    const method = 'get';
+    const paths = [
+      ["folderList", [{"from":0, "to":1}], ["id", "name", "parentId"]]
+    ];
+    const expectedResponse = {
+      "jsonGraph": {
+        "folderList": {
+          "0": { "$type": "ref", "value": [ "foldersById", 1 ] },
+          "1": { "$type": "ref", "value": [ "foldersById", 2 ] }
         },
         "foldersById": {
           "1": {
@@ -63,6 +89,7 @@ module.exports = () => {
         assert.end();
       });
   });
+
 
   test('["folderList", ...] Should return null for folders that do not exist', assert => {
     const method = 'get';
