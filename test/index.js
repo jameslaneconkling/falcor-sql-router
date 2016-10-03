0746c0928f4028b990bf33ee819e0dc96586a900
James Conkling <james.lane.conkling@gmail.com>
2016-10-03 14:51:27 -0400

    add test for set resourceById

--- test/index.js
+++ test/index.js
@@ -5,3 +5,4 @@ require('./getFolderList');
 require('./setFolderList');
 require('./callFolderList');
 require('./getResourceById');
+require('./setResourceById');
--- test/setResourceById.js
+++ test/setResourceById.js
@@ -11,20 +11,20 @@ const assertFailure = assert => err => {
 };
 
 
-test('foldersById: Should update folder name', assert => {
+test('resourcesById: Should update resource name', assert => {
   assert.plan(2);
   const model = setupFalcorTestModel(dbConstructor({seed: seedFilePath}));
   const expectedResponse = {
-    foldersById: {
+    resourcesById: {
       2: {
-        name: 'folder1 edit1'
+        name: 'green edit1'
       }
     }
   };
 
   model.set({
-    path: ['foldersById', 2, 'name'],
-    value: 'folder1 edit1'
+    path: ['resourcesById', 2, 'name'],
+    value: 'green edit1'
   })
     .subscribe(res => {
       assert.deepEqual(res.json, expectedResponse, 'set returns updated value');
@@ -32,9 +32,9 @@ test('foldersById: Should update folder name', assert => {
       // clear client cache, to ensure subsequent tests run against server db
       model.setCache({});
 
-      model.getValue(['foldersById', 2, 'name'])
+      model.getValue(['resourcesById', 2, 'name'])
         .subscribe(name => {
-          assert.equal(name, 'folder1 edit1', 'updated value is persisted');
+          assert.equal(name, 'green edit1', 'updated value is persisted');
         });
     }, assertFailure(assert));
 });
