4ca1ae4c337a712056d5f99b64763119549d9232
James Conkling <james.lane.conkling@gmail.com>
2016-10-04 22:23:59 -0400

    add create and delete resource routes

--- falcor/resourcesById.js
+++ falcor/resourcesById.js
@@ -1,4 +1,5 @@
 const Rx = require('rx');
+const R = require('ramda');
 const ResourceModelConstructor = require('../resource/resourceModel');
 
 module.exports = db => {
@@ -47,5 +48,49 @@ module.exports = db => {
           });
       }
     },
+    // CREATE Resource
+    {
+      route: 'resourcesById.createResource',
+      call(callPath, args) {
+        const newResources = args;
+
+        return Rx.Observable.from(newResources)
+          .flatMap(resource => {
+            // create resource
+            return Resource.create(resource.name, resource.folderId);
+          })
+          .map(resource => {
+            const resourcePathValues = Object.keys(resource).map(field => ({
+              path: ['resourcesById', resource.id, field],
+              value: resource[field]
+            }));
+
+            const resourceCollectionLengthPathValue = {
+              path: ['resourceList', 'length'],
+              invalidated: true
+            };
+
+            return [...resourcePathValues, resourceCollectionLengthPathValue];
+          });
+      }
+    },
+    // DELETE Resources by ID [implicit]
+    {
+      route: 'resourcesById[{keys:ids}].delete',
+      call(callPath) {
+        // resourceList is treated as an implicit dependency, so invalidation must be handled by client
+        return Resource.deleteByIds(callPath.ids)
+          .map(id => ([
+            {
+              path: ['resourcesById', id],
+              value: null
+            },
+            {
+              path: ['resourceList', 'length'],
+              invalidated: true
+            }
+          ]));
+      }
+    }
   ];
 };
--- test/index.js
+++ test/index.js
@@ -6,4 +6,7 @@ require('./integration/folder/setFolderList');
 require('./integration/folder/callFolderList');
 require('./integration/resource/getResourceById');
 require('./integration/resource/setResourceById');
+require('./integration/resource/callResourceById');
 require('./integration/resource/getResourceList');
+require('./integration/resource/setResourceList');
+require('./integration/resource/callResourceList');
